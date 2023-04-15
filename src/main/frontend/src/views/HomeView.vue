6bd3807840d20601229598932c05380a85f36caa
JohnnyRamirezSancho <info@johnnyramirez.es>
2023-04-15 15:15:41 +0200

    Init slider home view

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import NavBar from "../components/NavBar.vue";
+import SliderHome from "../components/SliderHome.vue"
 </script>
 
 <template>
   <NavBar></NavBar>
+  <SliderHome></SliderHome>
 </template>
